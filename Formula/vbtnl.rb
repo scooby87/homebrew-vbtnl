3e4a838312793ba1509a020b90a269230e1e919f
Алексей Артамонов <alexeyartamonov1987@gmail.com>
2026-03-24 15:25:23 +0300

    init vbtnl cli

---
+++ Formula/vbtnl.rb
@@ -0,0 +1,23 @@
+class Vbtnl < Formula
+  desc "Публичные HTTPS-туннели к localhost"
+  homepage "https://vbtnl.com"
+  version "0.1.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/vbtnl/vbtnl-releases/releases/download/v#{version}/vbtnl_#{version}_darwin_arm64.tar.gz"
+      sha256 "REPLACE_WITH_ARM64_SHA256"
+    else
+      url "https://github.com/vbtnl/vbtnl-releases/releases/download/v#{version}/vbtnl_#{version}_darwin_amd64.tar.gz"
+      sha256 "REPLACE_WITH_AMD64_SHA256"
+    end
+  end
+
+  def install
+    bin.install "vbtnl-client" => "vbtnl"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vbtnl --version")
+  end
+end
