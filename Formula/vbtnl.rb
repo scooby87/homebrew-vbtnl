3f985351837ea888ec5ad186b6717d95e590c913
Алексей Артамонов <alexeyartamonov1987@gmail.com>
2026-03-24 15:42:58 +0300

    changed version for tag

--- Formula/vbtnl.rb
+++ Formula/vbtnl.rb
@@ -1,14 +1,14 @@
 class Vbtnl < Formula
   desc "Публичные HTTPS-туннели к localhost"
   homepage "https://vbtnl.com"
-  version "0.1.0"
+  version "0.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/vbtnl/vbtnl-releases/releases/download/v#{version}/vbtnl_#{version}_darwin_arm64.tar.gz"
+      url "https://github.com/scooby87/vbtnl-releases/releases/download/v#{version}/vbtnl_#{version}_darwin_arm64.tar.gz"
       sha256 "REPLACE_WITH_ARM64_SHA256"
     else
-      url "https://github.com/vbtnl/vbtnl-releases/releases/download/v#{version}/vbtnl_#{version}_darwin_amd64.tar.gz"
+      url "https://github.com/scooby87/vbtnl-releases/releases/download/v#{version}/vbtnl_#{version}_darwin_amd64.tar.gz"
       sha256 "REPLACE_WITH_AMD64_SHA256"
     end
   end
